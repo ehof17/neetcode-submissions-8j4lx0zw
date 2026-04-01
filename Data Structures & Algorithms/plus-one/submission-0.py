a7cc0d23a9912e7955404bdd1fcfcd7b535de693
Elijah Hoffman <97199723+ehof17@users.noreply.github.com>
2026-03-31 19:45:37 -0700

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        res = []
+        for val in reversed(digits):
+            newVal = val + carry
+            carry, newVal = divmod(newVal, 10)
+            res.append(newVal)
+            print(f"carry is {carry}, newval is {newVal}")
+        if carry:
+            res.append(carry)
+        return list(reversed(res))
